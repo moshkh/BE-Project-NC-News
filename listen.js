1691e5aed4cd34bedcf3d1919617ae953342601f
Musab Hussain <moshkhussain@gmail.com>
2022-12-01 12:28:04 +0000

    configuring repo for hosting the database (on ElephantSQL) and the back-end (on Cyclic)

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 
-app.listen(8080, () => {
-  console.log("listening on port 8080");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
